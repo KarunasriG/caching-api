7021a45dbd7a1118aca9e90424a0d8a2a48ebdfa
KarunaSri Gaddam <gaddamkarunasri679@gmail.com>
2025-02-09 12:51:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// In memeory cache object
+const cache = new Map();
+const MAX_SIZE = process.env.MAX_CACHE_SIZE || 10;
+
+// Post Request /cache -> Stores a key-value pair
+app.post("/cache", (req, res) => {
+  const { key, value } = req.body;
+
+  if (!key || !value) {
+    return res.status(400).json({ message: "Key and Value are required" });
+  }
+
+  if (cache.size >= MAX_SIZE) {
+    return res.status(400).json({ message: "Cache is full" });
+  }
+
+  cache.set(key, value);
+  res.status(201).json({ message: "Item added to cache" }, key, value);
+});
+
+// GET Request /cache/:key -> Retrieves the value for a key
+app.get("/cache/:key", (req, res) => {
+  const key = req.params.key;
+
+  if (!key) {
+    return res.status(400).json({ message: "Key is required" });
+  }
+
+  if (cache.has(key)) {
+    return res
+      .status(200)
+      .json({ message: "Key found", value: cache.get(key) });
+  } else {
+    return res.status(404).json({ message: "Key not found" });
+  }
+});
+
+// DELETE Request /cache/:key -> Deletes a key-value pair
+app.delete("/cache/:key", (req, res) => {
+  const key = req.params.key;
+
+  if (!key) {
+    return res.status(400).json({ message: "Key is required" });
+  }
+
+  if (cache.has(key)) {
+    cache.delete(key);
+    return res.status(200).json({ message: "Key deleted" });
+  } else {
+    return res.status(404).json({ message: "Key not found" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
